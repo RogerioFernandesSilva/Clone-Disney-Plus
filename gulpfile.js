baf5d671086f0df0780f9d4a29ff0e89bd05a0b1
Rogério Fernandes <rog.fernandes@yahoo.com.br>
2025-03-27 14:11:26 -0300

    Ajustes no Js1.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // Exemplo se estiver usando SASS
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
-// Definindo a tarefa 'styles'
-gulp.task('styles', function() {
-    return gulp.src('src/styles/*.scss') // Caminho dos seus arquivos de estilo
-        .pipe(sass().on('error', sass.logError)) // Compila SASS para CSS
-        .pipe(gulp.dest('dist/css')); // Pasta de destino
-});
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
-// Definindo a tarefa 'watch'
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function() {
-    gulp.watch('src/styles/*.scss', gulp.series('styles'));
-};
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+}
