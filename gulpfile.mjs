2a30437ef7bd6df074f455a12c0d8971437edf28
Rogério Fernandes <rog.fernandes@yahoo.com.br>
2025-03-27 16:21:28 -0300

    Error no ERR_REQUIRE_ASYNC_MODULE

---
+++ gulpfile.mjs
@@ -0,0 +1,32 @@
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
+import imagemin from 'gulp-imagemin';
+import uglify from 'gulp-uglify';
+
+const sass = gulpSass(dartSass);
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+export default gulp.parallel(styles, images, scripts);
+
+export function watch() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+}
